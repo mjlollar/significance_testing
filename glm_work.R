2a207047f58328e1187284e18e2def8ffd75e6c6
Matthew Lollar <mjlollar1@gmail.com>
2023-03-03 17:06:44 -0600

    Update glm_work.R

--- glm_work.R
+++ glm_work.R
@@ -1,23 +1,51 @@
 setwd("/Users/matt/Desktop/paper_review/glm")
 df <- read.csv('glm_input_022123.csv', header=T, as.is=T)
+df <-read.csv('glm_input_1_success_022123.csv',header=T, as.is=T)
+
 
 library('arm')
+library('magritter')
+library('tidyverse')
+library('dplyr')
 
 df$maternal <- as.factor(df$maternal)
 df$paternal <- as.factor(df$paternal)
 df$type <- as.factor(df$type)
-#df$type <-relevel(df$type, ref="0")
 
 #Model 1
-glm.fit.1 <- glm(formula=status ~ type, data=df)
+glm.fit.1 <- glm(family="quasibinomial",formula=status ~ type, data=df)
 fit.1 <- summary(glm.fit.1)
 display(glm.fit.1,digits=3)
+
+#### https://www.science.smith.edu/~jcrouser/SDS293/labs/lab4-r.html
+glm_prob <- data.frame(probs = predict(glm.fit.1, type="response"))
+glm_pred <- glm_prob %>%
+  mutate(pred=ifelse(probs>.5,"1","0"))
+glm_pred = cbind(df, glm_pred)
+glm_pred %>%
+  count(pred, status) %>%
+  spread(status, n, fill=0)
+glm_pred %>%
+  summarize(score=mean(pred== status)) # Prediction rate
+####
+
 
 #Model 2
 glm.fit.2 <- glm(formula=status ~ type + maternal + paternal, data=df)
 fit.2 <- summary(glm.fit.2)
 display(glm.fit.2)
-fit.2
+
+#### https://www.science.smith.edu/~jcrouser/SDS293/labs/lab4-r.html
+glm_prob <- data.frame(probs = predict(glm.fit.2, type="response"))
+glm_pred <- glm_prob %>%
+  mutate(pred=ifelse(probs>.5,"1","0"))
+glm_pred = cbind(df, glm_pred)
+glm_pred %>%
+  count(pred, status) %>%
+  spread(status, n, fill=0)
+glm_pred %>%
+  summarize(score=mean(pred== status)) # Prediction rate
+####
 
 # Model 3
 glm.fit.3 <- glm(formula=status ~ type + maternal + paternal + maternal:paternal, data=df)
