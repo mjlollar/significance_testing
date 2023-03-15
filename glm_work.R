391e4556559e0b02cfe9a7efc9286ca303800198
Matthew Lollar <mjlollar1@gmail.com>
2023-03-15 16:34:21 -0500

    Update glm_work.R

--- glm_work.R
+++ glm_work.R
@@ -18,24 +18,26 @@ df$maternal <- as.factor(df$maternal)
 df$paternal <- as.factor(df$paternal)
 df$type <- as.factor(df$type)
 
-group_maternal <- aggregate(df$status,list(df$maternal),mean)
-group_paternal <- aggregate(df$status,list(df$paternal),mean)
-m <- mean(group_maternal$x)
-p <- mean(group_paternal$x)
-m_diff <- abs(group_maternal$x - m)
+group_maternal <- aggregate(df$status,list(df$maternal),mean) #maternal mean of each strain
+group_paternal <- aggregate(df$status,list(df$paternal),mean) #paternal mean of each strain
+m <- mean(group_maternal$x) #list of maternal means in index order below
+p <- mean(group_paternal$x) #list of paternal means in index order below
+m_diff <- abs(group_maternal$x - m) #list of the difference in strain mean and population mean
 p_diff <- abs(group_paternal$x - p)
-which.min(m_diff) # 8, Z2
-which.min(p_diff) # 8, Z2
+which.min(m_diff) #get index of lowest difference
+# 8, Z3 (ZI366N) 
+which.min(p_diff) 
+# 8, Z3 (ZI366N)
 
 # index position coords
 # 1  2  3  4  5  6   7  8  9  10
 # f1 f2 f3 f4 f5 zi1 z2 z3 z4 z5 
 
-which.min(m_diff) # 8, Z2
-which.min(p_diff) # 8, Z2
+which.min(m_diff) # 8, Z3
+which.min(p_diff) # 8, Z3
 
-df$maternal <- relevel(df$maternal, ref='Z2')
-df$paternal <- relevel(df$paternal, ref='Z2')
+df$maternal <- relevel(df$maternal, ref='Z3')
+df$paternal <- relevel(df$paternal, ref='Z3')
 
 glm.2 <- glm(family="quasibinomial", formula=status ~ type + maternal + paternal, data=df)
 summary(glm.2)
